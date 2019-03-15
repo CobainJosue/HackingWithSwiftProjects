203fcbdc4ecef8888a037c02d2138603836f4e97
CobainJosue <chivasagrada@live.com.mx>
2019-03-14 21:29:38 -0600

    Day 42 Project 10 part 1

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Josue Quiñones on 3/14/19.
+//  Copyright © 2019 Josue Quiñones. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Josue Quiñones on 3/14/19.
+//  Copyright © 2019 Josue Quiñones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+        return cell
+    }
+
+}
+
